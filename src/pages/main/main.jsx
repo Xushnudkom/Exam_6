1901066e31f6ee123a3539f72ae7642e02450479
Xushnudkom <xushnudkomilovv5681@gmail.com>
2024-06-10 01:08:05 +0500

    exam

---
+++ src/pages/main/main.jsx
@@ -0,0 +1,18 @@
+import ResponsiveDrawer from "../../components/layout"
+import "./main.css"
+const Main = () => {
+  return (
+    <div>
+      <ResponsiveDrawer />
+        {/* <Sidebar />
+        <div className="main-left">
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </div> */}
+    </div>
+  )
+}
+
+export default Main
